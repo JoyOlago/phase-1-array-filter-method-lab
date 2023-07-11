e35636639ae1bbcaab331c6b6023c8e0c0bdd5a8
Joy Olago <joydanielle62@gmail.com>
2023-07-11 09:44:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(list, name){
+    return list.filter(function (driverName){
+        return driverName.toUpperCase() === name.toUpperCase();
+    });
+}
+function fuzzyMatch(list, nameBegin){
+    let fullName = nameBegin.length;
+    return list.filter(function(driverName){
+        return driverName.slice(0, fullName) === nameBegin;
+    });
+
+}
+
+function matchName(list, name){
+
+    return list.filter(function(driver){
+        return driver[`name`].toLowerCase() === name.toLowerCase();
+    });
+  }
